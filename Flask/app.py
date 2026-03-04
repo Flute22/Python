77a2292074400d348fa063f3a3ba72f759e055f9
khushalsinhmar <shimarsabb@gmail.com>
2026-03-04 11:05:27 +0530

    feat: Add initial Flask application with user registration, login, and a profile page linking to NLP features.

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from db import Database
+
+dbo = Database()
 
 app = Flask(__name__)
 
@@ -18,7 +21,49 @@ def registration_data():
     user_email = request.form.get('email')
     user_password = request.form.get('password')
 
-    return f'Name: {user_name} Email: {user_email} Password: {user_password}'
+    response = dbo.insert(user_name, user_email, user_password)
+
+    if response:  
+        return render_template("login.html", message="Registration Successful. Kindly login to proceed.")
+    else: 
+        return render_template("register.html", message="Email already Exists!")
+
+
+@app.route('/perform-login', methods=['post'])
+def perform_login(): 
+    user_email = request.form.get('email')
+    user_password = request.form.get('password')
+
+    response = dbo.search(user_email, user_password)
+
+    if response: 
+        return redirect('/profile')
+    else: 
+        return render_template('login.html', message="Incorrect email/password")
+    
+
+@app.route('/profile')
+def profile(): 
+    return render_template('/profile.html')
+
+
+@app.route('/ner')
+def ner(): 
+    return render_template('ner.html')
+
+
+@app.route('/perform-ner', methods=['post'])
+def perform_ner(): 
+    text = request.form.get('ner_text')
+
+@app.route('/sentiment-analysis')
+def sentiment_analysis():
+    return 'sentiment analysis'
+
+
+@app.route('/abuse-detection')
+def abuse_detection(): 
+    return 'abuse detection'
 
 
 app.run(debug=True)
